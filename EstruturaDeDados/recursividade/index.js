eae546d5f501c198620aceeb63dd1d0a38a9b443
Igor Brenno Dantas <54316405+igorBrenno@users.noreply.github.com>
2024-11-28 19:52:12 +0000

    update

---
+++ EstruturaDeDados/recursividade/index.js
@@ -0,0 +1,5 @@
+// crie uma função recursiva para obter a multiplicação a partir de operações de soma
+
+// Crie uma função recursiva para obter a exponenciação a partir de operação de multiplicação
+
+// crie uma programa utilizando uma função para calcular a quantidade nascimentos de coelhos, de acordo com a sequência de fibonacci, após 12 meses
